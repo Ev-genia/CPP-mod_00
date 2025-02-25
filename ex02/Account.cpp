f1af291513cf4642f0008cda585e09dc670483b9
Ev-genia <eugenia.kostareva@outlook.com>
2025-02-25 21:30:27 +0400

    add Account class implementation and test suite

---
+++ ex02/Account.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 16:53:43 by mlarra            #+#    #+#             */
+/*   Updated: 2022/10/05 17:10:34 by mlarra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include "tests.cpp"
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+
